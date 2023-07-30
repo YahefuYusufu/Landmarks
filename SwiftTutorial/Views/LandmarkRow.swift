ffabdaa6839353cf38223e189bf38edd6ed5b4f7
prg0615 <parguzatt@gmail.com>
2023-07-31 00:46:06 +0200

    Building List and Navigation : 1 & 2 & 3 section have done , next step is section 4/

---
+++ SwiftTutorial/Views/LandmarkRow.swift
@@ -0,0 +1,36 @@
+//
+//  LandmarkRow.swift
+//  SwiftTutorial
+//
+//  Created by yusufyakuf on 2023-07-31.
+//
+import Foundation
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+
+
+            Spacer()
+        }
+    }
+}
+
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+            .previewLayout(.fixed(width: 300, height: 70))
+        LandmarkRow(landmark: landmarks[1])
+            .previewLayout(.fixed(width: 300, height: 70))
+        
+    }
+}
