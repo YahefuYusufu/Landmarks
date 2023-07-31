01ca999e775496c98005ec054eec0404694abb13
prg0615 <parguzatt@gmail.com>
2023-07-31 14:06:59 +0200

    section 7 has done

--- SwiftTutorial/Views/CircleImage.swift
+++ SwiftTutorial/Views/CircleImage.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image("image1").frame(width: 250)
-            .clipShape(Circle()).overlay{
+    image.clipShape(Circle()).overlay{
             Circle().stroke(.white,lineWidth: 4)
         }
         .shadow(radius: 7)
@@ -20,6 +21,6 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
