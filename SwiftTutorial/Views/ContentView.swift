d41844715809af8bb90fdded53ec5271015df43c
prg0615 <parguzatt@gmail.com>
2023-07-31 13:12:05 +0200

    section 6 has done

--- SwiftTutorial/Views/ContentView.swift
+++ SwiftTutorial/Views/ContentView.swift
@@ -9,29 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        LazyVStack {
-            MapView().ignoresSafeArea(edges: .top).frame(height: 350)
-            
-            CircleImage().offset(y:-210).padding(.bottom,-120)
-            
-            VStack(alignment: .leading) {
-                Text("Turtle Rock").font(.title)
-                
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
-                }.font(.subheadline)
-                    .foregroundColor(.secondary)
-                Divider()
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-        .padding()
-            
-        Spacer()
-    }
+        LandmarkList()
     }
 }
 
