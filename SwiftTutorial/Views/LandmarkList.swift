a70ba2e1f1b5eb0dd246651db966098c8faf330f
prg0615 <parguzatt@gmail.com>
2023-08-02 19:33:44 +0200

    2_section-5 Adopt the modal object in your views

--- SwiftTutorial/Views/LandmarkList.swift
+++ SwiftTutorial/Views/LandmarkList.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
     
+    
     var filteredLandmarks : [Landmark] {
-        landmarks.filter {landmark in
+        landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
+
     
     var body: some View {
         NavigationView{
