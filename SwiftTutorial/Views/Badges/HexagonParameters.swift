debe522ff6831ed9b34825135dd5f614f774647f
prg0615 <parguzatt@gmail.com>
2023-08-03 12:56:47 +0200

    Drawing & Animation create Drawing data for a Badge view in progress.

---
+++ SwiftTutorial/Views/Badges/HexagonParameters.swift
@@ -0,0 +1,19 @@
+//
+//  HexagonParameters.swift
+//  SwiftTutorial
+//
+//  Created by yusufyakuf on 2023-08-03.
+//
+
+import Foundation
+
+struct HexagonParameters {
+    struct Segment {
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+
+    }
+}
+
+
