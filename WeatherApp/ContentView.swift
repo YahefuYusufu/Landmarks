8a32ef648d379d30e5c889d1429b91ace5d8f78e
YahefuYusufu <parguzatt@gmail.com>
2023-09-26 21:09:59 +0200

    second message commit

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
         Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
             /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
         }
+        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+        }
     }
 }
 
