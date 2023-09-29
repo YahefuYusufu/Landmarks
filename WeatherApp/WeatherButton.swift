46a7195b005c3a7bfb0b9bbf477732d8d21959a7
YahefuYusufu <parguzatt@gmail.com>
2023-09-29 20:50:55 +0200

    some project organization

---
+++ WeatherApp/WeatherButton.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherButton.swift
+//  WeatherApp
+//
+//  Created by yusufyakuf on 2023-09-29.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherButton()
+}
