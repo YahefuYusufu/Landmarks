5cec1d050ccec977ab1c2c4e669ef72a70e2332f
YahefuYusufu <parguzatt@gmail.com>
2023-10-01 21:02:54 +0200

    Initial Commit

---
+++ apple_framworks/apple_framworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_framworksApp.swift
+//  apple_framworks
+//
+//  Created by yusufyakuf on 2023-10-01.
+//
+
+import SwiftUI
+
+@main
+struct apple_framworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
