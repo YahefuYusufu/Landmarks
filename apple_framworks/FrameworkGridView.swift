4ef5b4c20ab91b2986db6da0213dd29f4b8a5966
YahefuYusufu <parguzatt@gmail.com>
2023-10-01 21:33:34 +0200

    second project : next step is framworkGridView (Grid).

---
+++ apple_framworks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  apple_framworks
+//
+//  Created by yusufyakuf on 2023-10-01.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
