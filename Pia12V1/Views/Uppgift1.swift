cd2383a641c17e3a974544d9534b6ca2264f9f60
YahefuYusufu <parguzatt@gmail.com>
2023-10-31 19:18:23 +0100

    uppgift 01

---
+++ Pia12V1/Views/Uppgift1.swift
@@ -0,0 +1,18 @@
+//
+//  Uppgift1.swift
+//  Pia12V1
+//
+//  Created by yusufyakuf on 2023-10-31.
+//
+
+import SwiftUI
+
+struct Uppgift1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Uppgift1()
+}
