4b280edfe1f62352cb7dd4fe928d8ffe948246ec
YahefuYusufu <parguzatt@gmail.com>
2023-10-31 19:19:49 +0100

    uppgift 01

--- Pia12V1/Views/Uppgift1.swift
+++ Pia12V1/Views/Uppgift1.swift
@@ -9,9 +9,54 @@ import SwiftUI
 
 struct Uppgift1: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        VStack {
+            HStack {
+                       Color.red
+                           .frame(maxWidth: .infinity, maxHeight: 200)
+                           .overlay(
+                               Text("RED")
+                                   .foregroundColor(.white)
+                                   .multilineTextAlignment(.center)
+                                   .font(.title))
+                       
+                       Color.blue
+                           .frame(maxWidth: .infinity, maxHeight: 200)
+                           .overlay(
+                               Text("BLUE")
+                                   .foregroundColor(.white)
+                                   .multilineTextAlignment(.center)
+                                   .font(.title))
+               }
+                      VStack {
+                        Text("GREEN")
+               }
+                                  .foregroundColor(.white)
+                                  .frame(width: 400,height: 100)
+                                  .background(.green)
+                                  .font(.title)
+            Spacer()
+            
+            Color.red
+                .frame(width: 200,height: 200)
+                .padding(.bottom,0)
+                .overlay(
+                    Text("CIRCLE")
+                        .frame(width: 150,height: 150)
+                        .background(.blue)
+                        .cornerRadius(100)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .font(.title))
+                   }
+                        .background(.gray)
+                        
+   
+     }
+     
 }
+    
+    
+
 
 #Preview {
     Uppgift1()
