63b790417e7d24a992cd3248fe8b3ad6d585f648
YahefuYusufu <parguzatt@gmail.com>
2023-11-02 13:04:03 +0100

    week 2

--- Pia12iosv1tros/CodeView.swift
+++ Pia12iosv1tros/CodeView.swift
@@ -10,8 +10,42 @@ import SwiftUI
 struct CodeView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Button(action: {
+            displayText()
+        }, label: {
+            /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+        })
     }
 }
+
+func displayText(){
+    let meyText = "hey and welcome here"
+    let allWord = meyText.components(separatedBy: " ")
+    
+    var longestWord = 0
+    
+    for someWord in allWord {
+        if someWord.count > longestWord {
+            longestWord = someWord.count
+        }
+    }
+    
+    var startRow = ""
+    for _ in 1...(longestWord+4) {
+        startRow = startRow + "*"
+    }
+    
+    print(longestWord)
+    for someWord in allWord {
+        var someSpave = " "
+        print("* " + someWord + " *")
+    }
+    print(startRow)
+}
+
+
+
 
 #Preview {
     CodeView()
--- Pia12iosv1tros/ContentView.swift
+++ Pia12iosv1tros/ContentView.swift
@@ -8,14 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var resultNumber = 0
+    @State var inputNumber = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(String(resultNumber))
+                .font(.title)
+            TextField("Write nuber", text: $inputNumber)
+            HStack {
+                Button(action: {
+                    doCalc(count: "Plus")
+                }, label: {
+                    Text("Plus")
+                })
+                Button(action: {
+                    doCalc(count: "Minus")
+                }, label: {
+                    Text("Minus")
+                })
+                Button(action: {
+                    doCalc(count: "Multi")
+                }, label: {
+                    Text("Multi")
+                })
+            }
+            Spacer()
+            Button(action: {
+                doCalc(count: "Reset")
+            }, label: {
+                Text("Reset")
+            })
         }
         .padding()
+    }
+    
+    func doCalc(count: String){
+        let numbers = ["one","two","three","four","five"]
+        if let findNumber = numbers.firstIndex(where: { $0 == inputNumber}) {
+            if count == "Plus" {
+                resultNumber += findNumber
+            }
+            if count == "Minus" {
+                resultNumber -= findNumber
+            }
+            if count == "Minus" {
+                resultNumber = resultNumber * findNumber
+            }
+
+        }
+        
+        var textNumber = 0
+        if inputNumber.lowercased() == "one" {
+            textNumber = 1
+        }
+        if inputNumber.lowercased() == "two" {
+            textNumber = 2
+        }
+        if inputNumber.lowercased() == "three" {
+            textNumber = 3
+        }
+        if count == "Plus" {
+            resultNumber += textNumber
+        }
+        if count == "Minus" {
+            resultNumber -= textNumber
+        }
+        if count == "Plus" {
+            if let funNumber = Int(inputNumber) {
+                resultNumber += funNumber
+            }
+        }
+        if count == "Multi" {
+            resultNumber = resultNumber * textNumber
+        }
+
+        
+        //                if Int(inputNumber) != nil {
+        //                    resultNumber =
+        //                    resultNumber +
+        //                    Int(inputNumber)!
+        //                }
+        
     }
 }
 
