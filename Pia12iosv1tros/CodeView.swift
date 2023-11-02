1b1a61aa94ffff334d7b95572537c3776262a465
YahefuYusufu <parguzatt@gmail.com>
2023-11-02 13:02:21 +0100

    week 2

---
+++ Pia12iosv1tros/CodeView.swift
@@ -0,0 +1,18 @@
+//
+//  CodeView.swift
+//  Pia12iosv1tros
+//
+//  Created by yusufyakuf on 2023-11-02.
+//
+
+import SwiftUI
+
+struct CodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CodeView()
+}
