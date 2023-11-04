9d912e4ee3eb7609ebf1afe95c834abe4df6f687
YahefuYusufu <parguzatt@gmail.com>
2023-11-04 20:08:06 +0100

    new todo list model and save func has created

--- TodoApp/ViewModels/NewItemViewViewModel.swift
+++ TodoApp/ViewModels/NewItemViewViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel: ObservableObject {
     @Published var title = ""
@@ -17,7 +19,31 @@ class NewItemViewViewModel: ObservableObject {
     }
     
     func save() {
+        guard canSave else {
+            return
+        }
+        //Get current user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
         
+        //Create model
+        let newId = UUID().uuidString
+        let newItem = TodoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
+        
+        //Save model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("قىلىدىغان ئشلار")
+            .document(newId)
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
