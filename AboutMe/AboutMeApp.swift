1a3a1663f9e2b02c3d53533501523128732a9249
YahefuYusufu <parguzatt@gmail.com>
2023-11-07 06:56:47 +0100

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by yusufyakuf on 2023-11-07.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
