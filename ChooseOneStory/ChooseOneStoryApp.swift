cd0966c7db42fe3ccbf7b0cec97f655418331ce4
YahefuYusufu <parguzatt@gmail.com>
2023-11-07 09:41:22 +0100

    Initial Commit

---
+++ ChooseOneStory/ChooseOneStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseOneStoryApp.swift
+//  ChooseOneStory
+//
+//  Created by yusufyakuf on 2023-11-07.
+//
+
+import SwiftUI
+
+@main
+struct ChooseOneStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
