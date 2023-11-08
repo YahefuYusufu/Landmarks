f2f7166a8e8b39f75554f7e4b214738621397c80
YahefuYusufu <parguzatt@gmail.com>
2023-11-08 08:13:04 +0100

    Initial Commit

---
+++ DataPlanner/DataPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataPlannerApp.swift
+//  DataPlanner
+//
+//  Created by yusufyakuf on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct DataPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
