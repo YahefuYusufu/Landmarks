e248b429f3e9b54f434e51df755599d16276470f
YahefuYusufu <parguzatt@gmail.com>
2023-11-09 11:30:59 +0100

    navigationStack,navigation link in the morning!

--- pia12iosNavigation/ContentView.swift
+++ pia12iosNavigation/ContentView.swift
@@ -8,17 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+   @State var people = [Person]()
+   @State var addTodo = ""
+   @State var addLastName = ""
+   
+   
+   var body: some View {
+      NavigationStack {
+         VStack {
+            HStack {
+               TextField("Name",text:$addTodo)
+               TextField("LastName",text:$addLastName)
+               Button(action: {
+                  var tempPerson = Person(firstName: addTodo , lastName: addLastName)
+                  people.append(tempPerson)
+               }, label: {
+                  Text("ADD")
+               })
+            }
+            .padding()
+            .navigationTitle("personal")
+            
+            List(people,id: \.self.firstName) { people in
+               NavigationLink(
+                  destination: PersonDetailView(theName: people),
+                  label:  {
+                     ListRowView(personNane: people)
+                  })
+            }
+            .listStyle(.automatic)
+            
+         }
+         Spacer()
+      }
+      //            .onAppear() {
+      //                              print("hej")
+      //                              var someone = Person(firstName: "adil", lastName: "ekrem")
+      //                              print(someone.lastName)
+      //            }
+   }
 }
 
 #Preview {
-    ContentView()
+   ContentView()
 }
---
+++ pia12iosNavigation/PersonDetailView.swift
@@ -0,0 +1,37 @@
+//
+//  PersonDetailView.swift
+//  pia12iosNavigation
+//
+//  Created by yusufyakuf on 2023-11-09.
+//
+
+import SwiftUI
+
+struct PersonDetailView: View {
+   @Environment(\.dismiss) var dismiss
+   @State var theName : Person
+   @State var PName = ""
+    var body: some View {
+       Text("more about: " + theName.firstName)
+       
+       TextField("", text: $PName)
+       Button(action: {
+          dismiss()
+       }, label: {
+          Text("Save")
+       })
+       
+       NavigationLink(destination: ContentView(), label: {
+          Text("go back")
+       })
+             .onAppear() {
+                PName = theName.firstName
+             }
+    }
+   
+
+}
+
+#Preview {
+   PersonDetailView(theName: Person.init(firstName: "abc", lastName: "xyz"))
+}
