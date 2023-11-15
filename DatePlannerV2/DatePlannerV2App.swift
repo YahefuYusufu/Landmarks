13255cd7e91649702582ee597d5d57e71276932e
YahefuYusufu <parguzatt@gmail.com>
2023-11-15 09:44:12 +0100

    Initial Commit

---
+++ DatePlannerV2/DatePlannerV2App.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerV2App.swift
+//  DatePlannerV2
+//
+//  Created by yusufyakuf on 2023-11-15.
+//
+
+import SwiftUI
+
+@main
+struct DatePlannerV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
