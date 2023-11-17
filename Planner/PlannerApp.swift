34015eb4b6c0f633415c66b014a10f2f3bfe274c
YahefuYusufu <parguzatt@gmail.com>
2023-11-17 11:14:09 +0100

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by yusufyakuf on 2023-11-17.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
