cf9cec131d67c835c51312ae184aafbb15f4a3bf
YahefuYusufu <parguzatt@gmail.com>
2023-11-20 09:17:43 +0100

    Initial Commit

---
+++ PiaFirebase/PiaFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiaFirebaseApp.swift
+//  PiaFirebase
+//
+//  Created by yusufyakuf on 2023-11-20.
+//
+
+import SwiftUI
+
+@main
+struct PiaFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
