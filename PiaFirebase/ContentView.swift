e92b436acce3a6935d2cbdf703fb1539a4f9358f
YahefuYusufu <parguzatt@gmail.com>
2023-11-20 14:36:02 +0100

    has changed

--- PiaFirebase/ContentView.swift
+++ PiaFirebase/ContentView.swift
@@ -6,10 +6,11 @@
 //
 
 import SwiftUI
-import FirebaseDatabase
+import Firebase
 
 struct ContentView: View {
    @State var addtodo = ""
+   @State var todoItems = [TodoItem]()
    var body: some View {
       VStack {
          HStack {
@@ -21,26 +22,70 @@ struct ContentView: View {
                Text("Save")
             })
          }
+         
+         List {
+            ForEach(todoItems,id: \.self.title) { todo in
+               HStack {
+                  Text(todo.title)
+                  Image(systemName:todo.isdone ?  "pencil.circle" : "pencil.circle.fill")
+                  Spacer()
+                  
+//                  if todo.isdone {
+//                     Text("KLAR")
+//                  } else {
+//                     Text("INTE KLAR")
+//                  }
+               }
+            }
+         }
       }
       .padding()
-              .onAppear() {
-                 loadTodo()
-      //           doStuff()
-              }
+      .onAppear() {
+         loadTodo()
+         //           doStuff()
+      }
    }
    func saveTodo() {
+      
+      if addtodo == "" {
+         // VARNA TOMT
+         
+         return
+      }
       var ref: DatabaseReference!
       ref = Database.database().reference()
       
-      ref.child("todo").setValue(addtodo)
+      var todoThing = [String:Any]()
+      todoThing["todoTitle"] = addtodo
+      todoThing["todoDone"] = false
+      
+      ref.child("todoList").childByAutoId().setValue(todoThing)
+      
+      
    }
    func loadTodo() {
       var ref: DatabaseReference!
       ref = Database.database().reference()
-      ref.child("todo").getData(completion: { error, snapshot in
+      ref.child("todoList").getData(completion: { error, snapshot in
          
-         if let thetodo = snapshot?.value {
-            addtodo = thetodo as! String
+         //         if let thetodo = snapshot?.value {
+         //            addtodo = thetodo as! String
+         //         }
+         var allTheTodos = [TodoItem]()
+         
+         for todoChild in snapshot!.children {
+            let childSnap = todoChild as! DataSnapshot
+            print("EN TODO SAK")
+            
+            if let theTodo = childSnap.value as? [String : Any] {
+               let tempTodo = TodoItem()
+               tempTodo.title = theTodo["todoTitle"] as! String
+               tempTodo.isdone = theTodo["todoDone"] as! Bool
+               
+               allTheTodos.append(tempTodo)
+            }
+            
+            todoItems = allTheTodos
          }
       })
    }
@@ -61,4 +106,11 @@ struct ContentView: View {
 
 #Preview {
    ContentView()
+}
+
+class TodoItem {
+   var title = ""
+   var isdone:  Bool = false
+   
+   
 }
