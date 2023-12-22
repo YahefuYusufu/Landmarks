5c117d9c1c9a64de8cff23a0ac5a485aba642149
YahefuYusufu <parguzatt@gmail.com>
2023-12-22 17:02:25 +0100

    Initial Commit

---
+++ NavigationSplitView/NavigationSplitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSplitViewApp.swift
+//  NavigationSplitView
+//
+//  Created by yusufyakuf on 2023-12-22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSplitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
