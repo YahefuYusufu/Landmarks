64461aa286cd0098618b6cc3c9dec8db6cf68f2a
YahefuYusufu <parguzatt@gmail.com>
2023-12-26 11:38:40 +0100

    edit & update view in progress.

---
+++ CRUDOperation/EditBookView.swift
@@ -0,0 +1,18 @@
+//
+//  EditBookView.swift
+//  CRUDOperation
+//
+//  Created by yusufyakuf on 2023-12-26.
+//
+
+import SwiftUI
+
+struct EditBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditBookView()
+}
