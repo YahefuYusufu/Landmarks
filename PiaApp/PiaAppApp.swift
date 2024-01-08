8e944cf89734d5544d717b120e4d4eed5b4b6b25
YahefuYusufu <parguzatt@gmail.com>
2024-01-08 10:21:37 +0100

    Initial Commit

---
+++ PiaApp/PiaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiaAppApp.swift
+//  PiaApp
+//
+//  Created by yusufyakuf on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct PiaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
